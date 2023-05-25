c71ab63658ff3fbc101087f5c3e376c3c05b7387
changach-3000 <sharon.changach@student.moringaschool.com>
2023-05-25 20:32:57 +0300

    added background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Glass from './components/Glass';
 import Circle from './components/Circle';
+import Background from './components/Background';
 
 function App() {
   return (
     <div className="App">
       
-    <Glass />
-
+  <Background
+ />
     </div>
   );
 }
--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,7 +3,24 @@ import React from 'react'
 function Background() {
   return (
     <div>
-        
+         <div class="wrapper">
+		<span></span>
+		<span></span>
+		<span></span>
+		<span></span>
+		<span></span>
+		<span></span>
+		<span></span>
+		<span></span>
+		<span></span>
+		<span></span>
+</div>
+
+<div class="banner">
+	<div class="content">
+		<h2><b>Animation</b></h2>
+	</div>
+</div>
     </div>
   )
 }

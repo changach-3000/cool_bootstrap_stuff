8ec4cb78b2fbfc10e2a1c7e3a4668389c03e4b16
changach-3000 <sharon.changach@student.moringaschool.com>
2023-05-25 16:05:44 +0300

    added the circle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Circle from './components/Circle';
 
 function App() {
   return (
     <div className="App">
-     
+     <Circle />
     </div>
   );
 }
